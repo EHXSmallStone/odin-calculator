72d0a07dbfe6f14b2404df32383016a5664472e6
EHXSmallStone <127904795+EHXSmallStone@users.noreply.github.com>
2023-07-05 23:20:31 -0300

    Add three variables and the operate function

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const subtract = (...args) => args.reduce((total, current) => total - current);
 const multiply = (...args) => args.reduce((total, current) => total * current);
 
 const divide = (...args) => args.reduce((total, current) => total / current);
+
+let operator;
+let firstNumber;
+let secondNumber;
+
+const operate = (operator, firstNumber, secondNumber) => {
+  // if (operator == '+') {
+  //   return add(firstNumber, secondNumber);
+  // } else if (operator == '-') {
+  //   return subtract(firstNumber, secondNumber);
+  // } else if (operator == '*') {
+  //   return multiply(firstNumber, secondNumber);
+  // } else if (operator == '/') {
+  //   return divide(firstNumber, secondNumber);
+  // }
+  switch(operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+      break;
+
+    case '-':
+      return subtract(firstNumber, secondNumber);
+      break;
+    
+    case '*':
+      return multiply(firstNumber, secondNumber);
+      break;
+
+    case '/':
+      return divide(firstNumber, secondNumber);
+      break;
+  };
+};
